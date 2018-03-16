1b5d1def4c945720cce51fb1acf51ff3ef0672db
Paweł Stolecki <noigell@gmail.com>
2018-03-16 10:46:55 +0100

    poprawki zadanie-9-5

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
-window.onload = function() {
-	console.log('The page is fully loaded.');
-
-	var btn = document.getElementsByClassName('button');
+var btn = document.getElementsByClassName('button');
 	
-	console.log(btn);
-	console.log(btn.length);
+console.log(btn);
+console.log(btn.length);
 
-	for (i = 0; i < btn.length; i++){
-		console.log(btn[i].innerText);
-	}
+for (var i = 0; i < btn.length; i++){
+	console.log(btn[i].innerText);
 }
